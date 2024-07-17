071cdc824fd7c7471ba3173e6654a5b624d51d3c
Tigercrl <tigercrl@icloud.com>
2024-07-17 16:27:43 +0800

    fix: dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	compatibilityDate: '2024-04-03',
-	devtools: { enabled: true },
-	modules: [
-		'@nuxtjs/i18n',
-		'@nuxtjs/eslint-module',
-		'@pinia/nuxt',
-		'@element-plus/nuxt',
-		'@nuxt/eslint',
-		'@vueuse/nuxt',
-	],
-	app: {
-		head: {
-			noscript: [
-				{
-					innerHTML:
-						'<div class="noscript"><p>MCSL Future Web 的部分功能需要启用 JavaScript 才可使用！</p></div>',
-				},
-			],
-			link: [
-				{
-					rel: 'icon',
-					type: 'image/png',
-					href: '/assets/img/favicon.png',
-				},
-			],
-		},
-		pageTransition: {
-			name: 'page',
-			mode: 'out-in',
-		},
-	},
-	ssr: false,
+    compatibilityDate: '2024-04-03',
+    devtools: {enabled: true},
+    ssr: false,
+    modules: [
+        '@nuxtjs/i18n',
+        '@nuxtjs/eslint-module',
+        '@pinia/nuxt',
+        '@element-plus/nuxt',
+        '@nuxt/eslint',
+        '@vueuse/nuxt',
+    ],
+    app: {
+        head: {
+            noscript: [
+                {
+                    innerHTML:
+                        '<div class="noscript"><p>MCSL Future Web 的部分功能需要启用 JavaScript 才可使用！</p></div>',
+                },
+            ],
+            link: [
+                {
+                    rel: 'icon',
+                    type: 'image/png',
+                    href: '/assets/img/favicon.png',
+                },
+            ],
+        },
+        pageTransition: {
+            name: 'page',
+            mode: 'out-in',
+        },
+    },
 });
