ac39777cb5c551144ee2e10dd092fe2cf2827d27
Tigercrl <tigercrl@icloud.com>
2024-07-17 19:49:25 +0800

    feat: fallback locale

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,8 +1,9 @@
-export default defineI18nConfig(async () => ({
-	legacy: false,
-	locale: 'zh-CN',
-	messages: {
-		'en-US': (await import('./assets/i18n/en-US.json')).default,
-		'zh-CN': (await import('./assets/i18n/zh-CN.json')).default,
-	},
-}));
+export default {
+    legacy: false,
+    locale: usePreferredLanguages().value[0],
+    fallbackLocale: 'zh-CN',
+    messages: {
+        'en-US': (await import('./assets/i18n/en-US.json')).default,
+        'zh-CN': (await import('./assets/i18n/zh-CN.json')).default,
+    },
+};
