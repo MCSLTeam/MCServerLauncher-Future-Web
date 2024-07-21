1eeca88bf4f06de0d2e48b5b00b4f1bba9b67697
Tigercrl <tigercrl@icloud.com>
2024-07-21 23:19:32 +0800

    feat: encrypt username in token

--- server/utils/auth.ts
+++ server/utils/auth.ts
@@ -1,26 +1,26 @@
-import { md5 } from 'js-md5';
-import type { JwtPayload } from 'jsonwebtoken';
+import {md5} from 'js-md5';
+import type {JwtPayload} from 'jsonwebtoken';
 import jwt from 'jsonwebtoken';
-import { getConfig } from '~/server/utils/config';
+import {getConfig} from '~/server/utils/config';
 
 /**
  * @description 返回配置文件中的私钥
  */
 async function getSecret() {
-	return (await getConfig()).auth.secret;
+    return (await getConfig()).auth.secret;
 }
 
 /**
  * @description 获取所有用户
  */
 async function getUsers() {
-	const users: { [key: string]: any } | null =
-		await storage.getItem('users.json');
-	if (users == null || typeof users != 'object') {
-		await storage.setItem('users.json', {});
-		return getUsers();
-	}
-	return users;
+    const users: { [key: string]: any } | null =
+        await storage.getItem('users.json');
+    if (users == null || typeof users != 'object') {
+        await storage.setItem('users.json', {});
+        return getUsers();
+    }
+    return users;
 }
 
 /**
@@ -28,19 +28,19 @@ async function getUsers() {
  * @param username string
  */
 export async function hasUser(username: string) {
-	const users = await getUsers();
-	return Object.getOwnPropertyNames(users).includes(username);
+    const users = await getUsers();
+    return Object.getOwnPropertyNames(users).includes(username);
 }
 
 /**
  * @description 是否存在管理员
  */
 export async function hasAdmin() {
-	const users = await getUsers();
-	for (const key in users) {
-		if (users[key].permissions.includes('admin')) return true;
-	}
-	return false;
+    const users = await getUsers();
+    for (const key in users) {
+        if (users[key].permissions.includes('admin')) return true;
+    }
+    return false;
 }
 
 /**
@@ -51,17 +51,17 @@ export async function hasAdmin() {
  * @throws Error 用户已存在则抛出异常
  */
 export async function addUser(
-	username: string,
-	password: string,
-	permissions: string[],
+    username: string,
+    password: string,
+    permissions: string[],
 ) {
-	if (await hasUser(username)) throw new Error('用户已存在');
-	const users = await getUsers();
-	users[username] = {
-		permissions: permissions,
-		password: md5(password),
-	};
-	await storage.setItem('users.json', users);
+    if (await hasUser(username)) throw new Error('用户已存在');
+    const users = await getUsers();
+    users[username] = {
+        permissions: permissions,
+        password: md5(password),
+    };
+    await storage.setItem('users.json', users);
 }
 
 /**
@@ -69,8 +69,8 @@ export async function addUser(
  * @param username string
  */
 export async function getUser(username: string) {
-	const users = await getUsers();
-	return users[username];
+    const users = await getUsers();
+    return users[username];
 }
 
 /**
@@ -79,37 +79,40 @@ export async function getUser(username: string) {
  * @param rememberMe boolean
  */
 async function generateToken(username: string, rememberMe: boolean = false) {
-	const config = await getConfig();
-	const expire = rememberMe
-		? config.auth.rememberMeExpire
-		: config.auth.expire;
-	return jwt.sign(
-		{
-			username: username,
-		},
-		await getSecret(),
-		{
-			expiresIn: expire,
-		},
-	);
+    const config = await getConfig();
+    const expire = rememberMe
+        ? config.auth.rememberMeExpire
+        : config.auth.expire;
+    return jwt.sign(
+        {
+            username: md5(username),
+        },
+        await getSecret(),
+        {
+            expiresIn: expire,
+        },
+    );
 }
 
 /**
- * @description 通过token获得用户名字
+ * @description 通过token获取用户名字
  * @param token string
  */
 export async function getUsernameByToken(token: string) {
-	return new Promise<string>((resolve, reject) => {
-		getSecret()
-			.then((secret) => {
-				jwt.verify(token, secret, (err, decoded) => {
-					if (decoded && (<JwtPayload>decoded).username)
-						resolve((<JwtPayload>decoded).username);
-					reject(err ?? new Error('无效的Token'));
-				});
-			})
-			.catch((e) => reject(e ?? new Error('无效的Token')));
-	});
+    return new Promise<string>((resolve, reject) => {
+        getSecret()
+            .then((secret) => {
+                jwt.verify(token, secret, async(err, decoded) => {
+                    if (decoded && (<JwtPayload>decoded).username)
+                        for(const user in await getUsers()){
+                            if (md5(user) == (<JwtPayload>decoded).username)
+                                resolve(user);
+                        }
+                    reject(err ?? new Error('无效的Token'));
+                });
+            })
+            .catch((e) => reject(e ?? new Error('无效的Token')));
+    });
 }
 
 /**
@@ -118,26 +121,28 @@ export async function getUsernameByToken(token: string) {
  * @throws string 无效Token（过期、未知用户等）则抛出异常
  */
 export function getTokenExpire(token: string) {
-	return new Promise<string>((resolve, reject) => {
-		getSecret()
-			.then(async (secret) => {
-				if (!(await getUsers())[await getUsernameByToken(token)]) {
-					reject('未知用户');
-					return;
-				}
-				const decoded = jwt.verify(token, secret);
-				if (decoded && (<JwtPayload>decoded).exp) {
-					resolve(
-						new Date(
-							(<{ exp: number }>decoded).exp * 1000,
-						).toISOString(),
-					);
-				}
-			})
-			.catch((e) => {
-				reject(e.message);
-			});
-	});
+    return new Promise<string>((resolve, reject) => {
+        getSecret()
+            .then(async (secret) => {
+                try {
+                    await getUsernameByToken(token)
+                } catch (e) {
+                    reject('未知用户');
+                    return;
+                }
+                const decoded = jwt.verify(token, secret);
+                if (decoded && (<JwtPayload>decoded).exp) {
+                    resolve(
+                        new Date(
+                            (<{ exp: number }>decoded).exp * 1000,
+                        ).toISOString(),
+                    );
+                }
+            })
+            .catch((e) => {
+                reject(e.message);
+            });
+    });
 }
 
 /**
@@ -149,16 +154,15 @@ export function getTokenExpire(token: string) {
  * @throws Error 密码错误抛出异常
  */
 export async function login(
-	username: string,
-	password: string,
-	rememberMe: boolean,
+    username: string,
+    password: string,
+    rememberMe: boolean,
 ) {
-	const users = await getUsers();
-	if (users[username] && users[username].password == md5(password)) {
-		console.log('用户' + username + '已登录');
-		return await generateToken(username, rememberMe);
-	}
-	throw new Error('用户名或密码错误');
+    if (await hasUser(username) && (await getUser(username)).password == md5(password)) {
+        console.log('用户' + username + '已登录');
+        return await generateToken(username, rememberMe);
+    }
+    throw new Error('用户名或密码错误');
 }
 
 /**
@@ -166,8 +170,8 @@ export async function login(
  * @param username string
  */
 export async function deleteUser(username: string) {
-	const users = await getUsers();
-	users[username] = undefined;
+    const users = await getUsers();
+    users[username] = undefined;
 }
 
 /**
@@ -175,14 +179,14 @@ export async function deleteUser(username: string) {
  * @param permissionList string[]
  * @param permission string
  */
-export async function hasPermission(
-	permissionList: string[],
-	permission: string,
+function hasPermission(
+    permissionList: string[],
+    permission: string,
 ) {
-	for (const perm of permissionList) {
-		if (matchPermission(perm, permission)) return true;
-	}
-	return false;
+    for (const perm of permissionList) {
+        if (matchPermission(perm, permission)) return true;
+    }
+    return false;
 }
 
 /**
@@ -191,10 +195,10 @@ export async function hasPermission(
  * @param b 要匹配的权限
  */
 function matchPermission(a: string, b: string) {
-	const nodesA = a.split('.');
-	const nodesB = b.split('.');
-	for (let i = 0; i < nodesA.length; i++) {
-		if (nodesA[i] != '*' && nodesA[i] != nodesB[i]) return false;
-	}
-	return true;
+    const nodesA = a.split('.');
+    const nodesB = b.split('.');
+    for (let i = 0; i < nodesA.length; i++) {
+        if (nodesA[i] != '*' && nodesA[i] != nodesB[i]) return false;
+    }
+    return true;
 }
