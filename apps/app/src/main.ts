13e8a8603ce5bf580a6596d95db6608fa72d385e
Tigercrl <tigercrl@icloud.com>
2025-08-20 12:44:39 +0800

    [common] fix: workflows

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -7,12 +7,14 @@ import { load } from "@repo/shared/src";
 import router from "./router.ts";
 import FloatingVue from "floating-vue";
 
-const app = createApp(App);
-const pinia = createPinia();
-app.use(FloatingVue);
-app.use(pinia);
-app.use(router);
-const i18n = createI18n((await useLocale().generateConfig()) as I18nOptions);
-app.use(i18n);
-load(<any>i18n.global);
-app.mount("#app");
+(async () => {
+  const app = createApp(App);
+  const pinia = createPinia();
+  app.use(FloatingVue);
+  app.use(pinia);
+  app.use(router);
+  const i18n = createI18n((await useLocale().generateConfig()) as I18nOptions);
+  app.use(i18n);
+  load(<any>i18n.global);
+  app.mount("#app");
+})();
