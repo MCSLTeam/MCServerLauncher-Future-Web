bf78507f7cf3710e6adb0cdc3ef5920aaabccc9c
Tigercrl <tigercrl@icloud.com>
2025-08-20 14:07:24 +0800

    [common] fix: workflows

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -3,7 +3,6 @@ import vue from "@vitejs/plugin-vue";
 import svgLoader from "vite-svg-loader";
 import dynamicImport from "vite-plugin-dynamic-import";
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
