491724da5fc17fcfc2f0f027fbedc5197ef7e13c
Tigercrl <tigercrl@icloud.com>
2025-09-27 19:15:02 +0800

    [shared] feat: Layout - Auth

---
+++ packages/shared/src/layouts/AuthLayout.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import FancyBackground from "@repo/ui/src/components/misc/FancyBackground.vue";
+import Panel from "@repo/ui/src/components/panel/Panel.vue";
+import { useI18n } from "vue-i18n";
+import { getPlatform } from "../index.ts";
+
+const t = useI18n().t;
+</script>
+
+<template>
+  <FancyBackground>
+    <div class="logo">
+      <img src="../assets/MCSL.png" alt="" />
+      <h1>
+        {{ t("shared.app.name.abbr") }} {{ t("shared.app.name.future") }}
+        {{ t(`${getPlatform()}.app.name.suffix`) }}
+      </h1>
+      <h2>{{ t("shared.app.desc") }}</h2>
+    </div>
+    <Panel class="auth-panel" body-class="auth-body">
+      <slot />
+    </Panel>
+  </FancyBackground>
+</template>
+
+<style scoped lang="scss">
+.logo {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: var(--mcsl-spacing-2xs);
+  margin-bottom: var(--mcsl-spacing-2xl);
+
+  & > img {
+    width: 7.5rem;
+  }
+
+  & > h1 {
+    color: transparent;
+    background: linear-gradient(
+      45deg,
+      var(--mcsl-color-green),
+      var(--mcsl-color-blue)
+    );
+    background-clip: text;
+  }
+
+  & > h2 {
+    color: var(--mcsl-text-color-gray);
+    font-size: var(--mcsl-font-size-xl);
+    font-weight: var(--mcsl-font-weight-base);
+  }
+}
+
+.auth-panel {
+  width: min(30rem);
+
+  @media (max-width: 768px) {
+    width: calc(100% - 2 * var(--mcsl-spacing-lg) - 2 * var(--mcsl-spacing-md));
+  }
+}
+</style>
+
+<style lang="scss">
+.auth-body {
+  & > h2 {
+    width: 100%;
+    text-align: center;
+    margin-bottom: var(--mcsl-spacing-sm);
+
+    color: var(--mcsl-text-color-secondary);
+    font-size: var(--mcsl-font-size-xl);
+    font-weight: var(--mcsl-font-weight-base);
+  }
+}
+</style>
