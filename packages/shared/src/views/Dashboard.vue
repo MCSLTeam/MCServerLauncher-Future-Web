360c74ba7288b19a25e58ee940297a7d3e0b1a76
Tigercrl <tigercrl@icloud.com>
2026-01-23 18:00:12 +0800

    [shared] feat: file core type detect

--- packages/shared/src/views/Dashboard.vue
+++ packages/shared/src/views/Dashboard.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { usePageData } from "../utils/stores.ts";
 import { useI18n } from "vue-i18n";
+import { getExecutableType } from "../utils/node/types/instanceInstallationType.ts";
+import { MCSLNotif } from "@repo/ui/src/utils/notifications.ts";
 
 usePageData().set({
   layout: "dashboard",
@@ -11,8 +13,29 @@ usePageData().set({
     },
   ],
 });
+
+async function handleFileChange(event: Event) {
+  const target = event.target as HTMLInputElement;
+  const file = target.files?.[0];
+  if (file) {
+    const content = await file.arrayBuffer();
+    const executableType = await getExecutableType(file.type, content);
+    new MCSLNotif({
+      data: {
+        title: "核心检测结果",
+        message: executableType,
+      },
+    }).open();
+  }
+}
 </script>
 
-<template></template>
+<template>
+  <input
+    type="file"
+    @change="handleFileChange"
+    placeholder="选择核心文件以检测"
+  />
+</template>
 
 <style scoped lang="scss"></style>
